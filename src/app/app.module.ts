2a461ee8f0d31f92127d05b514260135f487a2d0
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2023-01-27 14:09:54 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { LayoutComponent } from './layout/layout.component';
+
+//////////
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
+    ZXingScannerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
